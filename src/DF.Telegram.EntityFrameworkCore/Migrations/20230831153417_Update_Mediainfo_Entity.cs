38bcee120b05ab937340c0e3af6606c74eb8ce39
df123 <hws184433274hws@live.com>
2023-08-31 23:53:33 +0800

    Refactor database schema and migrations
    - Add a new migration file for updating the `AppMediaInfo` entity
    - Remove unnecessary columns and rename a column in the `AppMediaInfo` table
    - Define schema, properties, and relationships for multiple tables in the database

---
+++ src/DF.Telegram.EntityFrameworkCore/Migrations/20230831153417_Update_Mediainfo_Entity.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DF.Telegram.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMediainfoEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDownload",
+                table: "AppMediaInfo");
+
+            migrationBuilder.DropColumn(
+                name: "IsReturn",
+                table: "AppMediaInfo");
+
+            migrationBuilder.RenameColumn(
+                name: "TaskComplete",
+                table: "AppMediaInfo",
+                newName: "Title");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MimeType",
+                table: "AppMediaInfo",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MimeType",
+                table: "AppMediaInfo");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "AppMediaInfo",
+                newName: "TaskComplete");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDownload",
+                table: "AppMediaInfo",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReturn",
+                table: "AppMediaInfo",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
