70cb309f8429aa94fe4d448ff12c00fe49bc56b8
df123 <hws184433274hws@live.com>
2023-09-06 22:58:46 +0800

    feat: Set up Vue.js project with webpack configuration
    - Add package.json, tsconfig.json, and webpack.config.js files for DF.Telegram.VueApp
    - Configure webpack to handle TypeScript, CSS, font, SVG, and Vue files
    - Create Vue component `App.vue` that renders a button and updates count when clicked
    - Modify `Index.cshtml` to include new script and div with id "app"
    - Ignore `yarn.lock` and `dist` directory in Git
    - Mount `App` component to element with id `#app` in `main.ts`
    - Add necessary dependencies and dev dependencies for Vue.js and webpack

---
+++ src/DF.Telegram.Web/VueApp/webpack.config.js
@@ -0,0 +1,48 @@
+﻿const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader/dist/index')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { DefinePlugin } = require('webpack')
+
+module.exports = {
+    entry: {
+        main: './src/main.ts'
+    },
+    output: {
+        filename: '[name].entry.js',
+        path: path.resolve(__dirname, '..', 'wwwroot', 'dist')
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
+                type: 'asset'
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader',
+            },
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CleanWebpackPlugin(),
+        new DefinePlugin({
+            __VUE_PROD_DEVTOOLS__: false,
+            __VUE_OPTIONS_API__: false,
+        }),
+    ]
+};
