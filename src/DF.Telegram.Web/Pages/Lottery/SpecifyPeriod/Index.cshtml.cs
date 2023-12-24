ad3a0757a680e03dc80b9f7757f967687d870928
df123 <hws184433274hws@live.com>
2023-12-24 17:12:03 +0800

    添加指定期号计算和复式导入。

---
+++ src/DF.Telegram.Web/Pages/Lottery/SpecifyPeriod/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DF.Telegram.Web.Pages.Lottery.SpecifyPeriod
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
