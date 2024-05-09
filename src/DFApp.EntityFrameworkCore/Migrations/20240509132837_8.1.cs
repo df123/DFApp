c5d07be1e01302fc5e7eb7b8a8fb35b7cbbfefe1
df123 <hws184433274hws@live.com>
2024-05-09 21:32:31 +0800

    添加数据库修改

---
+++ src/DFApp.EntityFrameworkCore/Migrations/20240509132837_8.1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DFApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class _81 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "AbpTenants",
+                type: "TEXT",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpTenants_NormalizedName",
+                table: "AbpTenants",
+                column: "NormalizedName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AbpTenants_NormalizedName",
+                table: "AbpTenants");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "AbpTenants");
+        }
+    }
+}
