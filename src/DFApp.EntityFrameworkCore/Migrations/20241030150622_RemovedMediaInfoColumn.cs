66f5aba06ad304361c7f26a25b1e0f1bda0df0d9
df123 <hws184433274hws@live.com>
2024-10-31 22:55:52 +0800

    移除MediaInfo表中的MD5列并添加IsDownloadCompleted列
    - 移除MediaInfo表中的CreatorId列
    - 移除MediaInfo表中的ExtraProperties列
    - 移除MediaInfo表中的LastModifierId列
    - 移除MediaInfo表中的MD5列
    - 添加MediaInfo表中的IsDownloadCompleted列
    
    Generated by DeepSeek

---
+++ src/DFApp.EntityFrameworkCore/Migrations/20241030150622_RemovedMediaInfoColumn.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DFApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedMediaInfoColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "AppMediaInfo");
+
+            migrationBuilder.DropColumn(
+                name: "ExtraProperties",
+                table: "AppMediaInfo");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifierId",
+                table: "AppMediaInfo");
+
+            migrationBuilder.DropColumn(
+                name: "MD5",
+                table: "AppMediaInfo");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDownloadCompleted",
+                table: "AppMediaInfo",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDownloadCompleted",
+                table: "AppMediaInfo");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatorId",
+                table: "AppMediaInfo",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExtraProperties",
+                table: "AppMediaInfo",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastModifierId",
+                table: "AppMediaInfo",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MD5",
+                table: "AppMediaInfo",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
