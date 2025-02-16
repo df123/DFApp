e2c1db76ffe48fe4643619e7ce3eb38a2747700d
df123 <hws184433274hws@live.com>
2025-02-16 11:04:31 +0800

    新增彩票模拟相关DTO和服务接口

---
+++ src/DFApp.Application.Contracts/Lottery/Simulation/ILotterySimulationService.cs
@@ -0,0 +1,14 @@
+using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace DFApp.Lottery.Simulation
+{
+    public interface ILotterySimulationService : ICrudAppService<
+        LotterySimulationDto,
+        long,
+        PagedAndSortedResultRequestDto,
+        CreateUpdateLotterySimulationDto>
+    {
+    }
+}
