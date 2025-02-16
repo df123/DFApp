39241470038527ae4376b3bd355b4c984f5c24c7
df123 <hws184433274hws@live.com>
2025-02-16 12:56:55 +0800

    新增彩票模拟表及其数据库配置

---
+++ src/DFApp.EntityFrameworkCore/Migrations/20250216045323_AddLotterySimulation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DFApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLotterySimulation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppLotterySimulation",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TermNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    Number = table.Column<int>(type: "INTEGER", nullable: false),
+                    BallType = table.Column<int>(type: "INTEGER", nullable: false),
+                    GameType = table.Column<int>(type: "INTEGER", nullable: false),
+                    GroupId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ExtraProperties = table.Column<string>(type: "TEXT", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "TEXT", maxLength: 40, nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppLotterySimulation", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppLotterySimulation_TermNumber_GroupId",
+                table: "AppLotterySimulation",
+                columns: new[] { "TermNumber", "GroupId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppLotterySimulation");
+        }
+    }
+}
