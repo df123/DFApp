781e16b91c3d26383e2e7b6ffc7baacce5f78768
df123 <hws184433274hws@live.com>
2025-02-16 09:48:15 +0800

    重构彩票统计页面，添加选择框和图表功能，移除Vue实现

--- src/DFApp.Web/Pages/Lottery/Statistics/Index.cshtml.cs
+++ src/DFApp.Web/Pages/Lottery/Statistics/Index.cshtml.cs
@@ -1,12 +1,39 @@
+using DFApp.Lottery;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
 
 namespace DFApp.Web.Pages.Lottery.Statistics
 {
     public class IndexModel : PageModel
     {
+        private readonly ILotteryService _lotteryAppService;
+
+        public List<SelectListItem> LotteryTypes { get; set; }
+
+        public IndexModel(ILotteryService lotteryService)
+        {
+            _lotteryAppService = lotteryService;
+        }
+
         public void OnGet()
         {
+            var types = _lotteryAppService.GetLotteryConst();
+            LotteryTypes = types.Select(t => new SelectListItem
+            {
+                Value = t.LotteryTypeEng,
+                Text = t.LotteryType
+            }).ToList();
+        }
+
+        public async Task<JsonResult> OnGetStatisticsDataAsync(string purchasedPeriod, string winningPeriod, string lotteryType)
+        {
+            var data = await _lotteryAppService.GetStatisticsWin(purchasedPeriod, winningPeriod, lotteryType);
+            return new JsonResult(data);
         }
     }
 }
