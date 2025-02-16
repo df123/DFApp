c69198517eb8facb4207db81fa2528c4f64f44d0
df123 <hws184433274hws@live.com>
2025-02-16 11:01:04 +0800

    新增彩票球类型和彩票游戏类型枚举，添加模拟购买彩票类

---
+++ src/DFApp.Domain.Shared/Lottery/LotteryBallType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DFApp.Lottery
+{
+    /// <summary>
+    /// 彩票球类型
+    /// </summary>
+    public enum LotteryBallType
+    {
+        Red,
+        Blue
+    }
+
+}
---
+++ src/DFApp.Domain/Lottery/LotterySimulation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp;
+
+namespace DFApp.Lottery
+{
+    /// <summary>
+    /// 模拟购买彩票
+    /// </summary>
+    public class LotterySimulation : AuditedAggregateRoot<long>
+    {
+        /// <summary>
+        /// 期号
+        /// </summary>
+        public int TermNumber { get; set; }
+        /// <summary>
+        /// 号码
+        /// </summary>
+        public int Number { get; set; }
+        /// <summary>
+        /// 彩票球类型
+        /// </summary>
+        public required LotteryBallType BallType { get; set; }
+        /// <summary>
+        /// 彩票类型
+        /// </summary>
+        public required LotteryGameType GameType { get; set; }
+        /// <summary>
+        /// 分组ID
+        /// </summary>
+        public int GroupId { get; set; }
+    }
+}
