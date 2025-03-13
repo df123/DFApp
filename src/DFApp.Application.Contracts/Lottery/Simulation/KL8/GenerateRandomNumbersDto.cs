0e88b3fff49f27d7a87e83651d5363645d1a6153
df123 <hws184433274hws@live.com>
2025-03-13 23:12:29 +0800

    更新GenerateRandomNumbersDto，设置默认值以优化随机号码生成逻辑

--- src/DFApp.Application.Contracts/Lottery/Simulation/KL8/GenerateRandomNumbersDto.cs
+++ src/DFApp.Application.Contracts/Lottery/Simulation/KL8/GenerateRandomNumbersDto.cs
@@ -14,14 +14,14 @@ namespace DFApp.Lottery.Simulation.KL8
         [Required]
         [Range(1, 1000)]
         [Display(Name = "LotterySimulation:Generate:Count")]
-        public int Count { get; set; }
+        public int Count { get; set; } = 200;
 
         /// <summary>
         /// 彩票类型
         /// </summary>
         [Required]
         [Display(Name = "LotterySimulation:Generate:GameType")]
-        public LotteryGameType GameType { get; set; }
+        public LotteryGameType GameType { get; set; } = LotteryGameType.快乐8;
 
         /// <summary>
         /// 期号 (格式：yyyyxxx，例如：2023001)
