e1355ed451fcc8aa00e1799197ec3bc369e6b790
df123 <hws184433274hws@live.com>
2025-10-12 23:06:01 +0800

    refactor(app): 移除多个功能模块
    - 移除 Aria2、Bookkeeping、Configuration、Lottery、TG 等模块相关文件
    - 清理后端菜单配置和映射关系
    - 简化应用结构，保留核心框架

--- src/DFApp.Web/DFAppWebAutoMapperProfile.cs
+++ src/DFApp.Web/DFAppWebAutoMapperProfile.cs
@@ -11,20 +11,5 @@ public class DFAppWebAutoMapperProfile : Profile
     public DFAppWebAutoMapperProfile()
     {
         //Define your AutoMapper configuration here for the Web project.
-        CreateMap<MediaInfoDto, CreateUpdateMediaInfoDto>();
-
-        CreateMap<Pages.Bookkeeping.Category.CreateModalModel.CreateCategoryViewModel, CreateUpdateBookkeepingCategoryDto>();
-        CreateMap<BookkeepingCategoryDto, CreateUpdateBookkeepingCategoryDto>();
-
-        CreateMap<Pages.Bookkeeping.Expenditure.CreateModalModel.CreateExpenditureViewModel, CreateUpdateBookkeepingExpenditureDto>();
-        CreateMap<BookkeepingExpenditureDto, CreateUpdateBookkeepingExpenditureDto>();
-
-        CreateMap<Pages.Bookkeeping.Expenditure.EditModalModel.UpdateExpenditureViewModel, CreateUpdateBookkeepingExpenditureDto>();
-        CreateMap<BookkeepingExpenditureDto, Pages.Bookkeeping.Expenditure.EditModalModel.UpdateExpenditureViewModel>();
-
-        CreateMap<Pages.Configuration.CreateModalModel.CreateConfigurationInfoViewModel, CreateUpdateConfigurationInfoDto>();
-        CreateMap<Pages.Configuration.EditModalModel.UpdateConfigurationInfoViewModel, CreateUpdateConfigurationInfoDto>();
-        CreateMap<ConfigurationInfoDto, Pages.Configuration.EditModalModel.UpdateConfigurationInfoViewModel>();
-
     }
 }
--- src/DFApp.Web/Menus/DFAppMenus.cs
+++ src/DFApp.Web/Menus/DFAppMenus.cs
@@ -5,39 +5,6 @@ public class DFAppMenus
     private const string Prefix = "DFApp";
     public const string Home = Prefix + ".Home";
 
-    public static class Media
-    {
-        public const string Default = Prefix + ".Media";
-        public const string ExternalLink = Default + ".ExternalLink";
-        public const string Chart = Default + ".Chart";
-        public const string Login = Default + ".Login";
-    }
-
-    public const string DynamicIP = Prefix + ".DynamicIP";
-    public const string Lottery = Prefix + ".Lottery";
-    public const string LotterySimulation = Lottery + ".Simulation";
-    public const string LogSink = Prefix + ".LogSink";
-    public const string Bookkeeping = Prefix + ".Bookkeeping";
-    public const string FileUploadDownload = Prefix + ".FileUploadDownload";
-    public const string LogViewer = Prefix + ".LogViewer";
-
-    public static class ConfigurationInfo
-    {
-        public const string Default = Prefix + ".ConfigurationInfo";
-        public const string Create = Default + ".Create";
-        public const string Edit = Default + ".Edit";
-    }
-
-    public static class ManagementBackground
-    {
-        public const string Default = Prefix + ".ManagementBackground";
-    }
-
-    public static class Aria2
-    {
-        public const string Default = Prefix + ".Aria2";
-    }
-
     //Add your menu items here...
 
 }
