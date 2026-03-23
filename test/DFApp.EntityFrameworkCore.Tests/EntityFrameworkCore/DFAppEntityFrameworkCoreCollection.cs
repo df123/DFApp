91a4cd941826c25e7db2c98fc7f4cbc13eefc6bd
df123 <hws184433274hws@live.com>
2026-03-23 13:59:34 +0800

    chore(test): 移除示例测试项目及测试基类

--- test/DFApp.EntityFrameworkCore.Tests/EntityFrameworkCore/DFAppEntityFrameworkCoreCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xunit;
-
-namespace DFApp.EntityFrameworkCore;
-
-[CollectionDefinition(DFAppTestConsts.CollectionDefinitionName)]
-public class DFAppEntityFrameworkCoreCollection : ICollectionFixture<DFAppEntityFrameworkCoreFixture>
-{
-
-}
